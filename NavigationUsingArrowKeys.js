7f22437d7a394ceb07edeb90c0c2704015d3c354
JayantK0069 <kjayant907@gmail.com>
2025-08-19 12:54:37 +0530

    Add files via upload
    This enables the user to navigate through suggestion box using arrow keys and select them.
    It is required to include this file in the Sugg_SearchBar.html

---
+++ NavigationUsingArrowKeys.js
@@ -0,0 +1,37 @@
+let count2 = -1;
+document.addEventListener("keyup", (event) => {
+  const rows = document.querySelectorAll(".tabHolder tr td");
+  if (rows.length === 0) return;
+  if (event.key === "ArrowDown") {
+    count2++;
+    if (count2 >= 5) {
+      count2 = 4;
+    }
+    rows[count2].style.backgroundColor = "lightblue";
+    rows[count2].setAttribute("class", "main")
+    rows.forEach((i, j) => {
+      if (j != count2) {
+        i.style.backgroundColor = "#D3D3D3";
+        i.setAttribute("class", `rest`)
+      }
+    });
+  } else if (event.key === "ArrowUp") {
+    --count2;
+    if (count2 < 0) {
+      count2 = 0;
+    }
+    rows[count2].style.backgroundColor = "lightblue";
+    rows[count2].setAttribute("class", "main")
+    rows.forEach((i, j) => {
+      if (j != count2) {
+        i.style.backgroundColor = "#D3D3D3";
+        i.setAttribute("class", `rest`)
+      }
+    });
+  }
+  if (event.key === "Enter") {
+    if (rows[count2].className === "main" && rows[count2]) {
+      inputField.value = rows[count2].innerText;
+    }
+  }
+});
